c5f1a4ad080a5d0673207ab47fa9ec0171678487
Cauã Carvalho <cc.cauacarvalho@gmail.com>
2025-09-18 14:35:58 -0300

    Versão 1

---
+++ Mutatbase/v1.py
@@ -0,0 +1,7 @@
+#"GAGCCTACTAACGGGAT"
+Amotras = input(str("Sequência: "))
+if Amotras == "GAGCCTACTAACGGGAT":
+    print(f"A sequência ({Amotras}) não apresenta mutações.")
+else:
+    print(f"A sequência ({Amotras}) apresenta mutações.")
+
